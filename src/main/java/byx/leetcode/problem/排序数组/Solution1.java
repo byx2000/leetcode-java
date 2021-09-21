8bdac8a06b9dca3823560538d4fae2373f89283d
byx2000 <416982860@qq.com>
2021-09-21 14:13:16 +0800

    添加解法：排序数组

--- src/main/java/byx/leetcode/problem/排序数组/Solution1.java
+++ src/main/java/byx/leetcode/problem/排序数组/Solution1.java
@@ -1,15 +1,18 @@
 package byx.leetcode.problem.排序数组;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * https://leetcode-cn.com/problems/sort-an-array/
  */
-public class Solution {
+public class Solution1 {
     public int[] sortArray(int[] nums) {
         qsort(nums, 0, nums.length - 1);
         return nums;
     }
+
+    private static final Random RANDOM = new Random();
 
     private void qsort(int[] nums, int left, int right) {
         if (left >= right) {
@@ -28,18 +31,22 @@ public class Solution {
     }
 
     private int partition(int[] nums, int left, int right) {
+        swap(nums, left, left + RANDOM.nextInt(right - left + 1));
+
         int i = left + 1, j = right;
         while (i <= j) {
-            while (i <= j && nums[i] < nums[left]) i++;
-            while (i <= j && nums[j] >= nums[left]) j--;
-            if (i < j) swap(nums, i, j);
+            while (i <= j && nums[i] <= nums[left]) i++;
+            while (i <= j && nums[j] > nums[left]) j--;
+            if (i < j) {
+                swap(nums, i, j);
+            }
         }
         swap(nums, left, j);
         return j;
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution1 solution = new Solution1();
         System.out.println(Arrays.toString(solution.sortArray(new int[]{5, 2, 3, 1})));
         System.out.println(Arrays.toString(solution.sortArray(new int[]{5,1,1,2,0,0})));
     }
