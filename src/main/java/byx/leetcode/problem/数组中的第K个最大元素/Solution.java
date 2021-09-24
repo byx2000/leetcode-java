8bb8542527e8c2384c0d53948781c241c6182290
byx2000 <416982860@qq.com>
2021-09-24 10:51:00 +0800

    添加题目：数组中的第K个最大元素

---
+++ src/main/java/byx/leetcode/problem/数组中的第K个最大元素/Solution.java
@@ -0,0 +1,29 @@
+package byx.leetcode.problem.数组中的第K个最大元素;
+
+import java.util.PriorityQueue;
+
+/**
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+ */
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            if (pq.size() < k) {
+                pq.add(n);
+            } else {
+                if (n > pq.peek()) {
+                    pq.remove();
+                    pq.add(n);
+                }
+            }
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        System.out.println(solution.findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+    }
+}
