4461e641a8506ff1171e42f986884299fe1ab102
byx2000 <416982860@qq.com>
2021-09-27 19:25:11 +0800

    修改题目：数组中的第K个最大元素

--- src/main/java/byx/leetcode/problem/数组中的第K个最大元素/Solution.java
+++ src/main/java/byx/leetcode/problem/数组中的第K个最大元素/Solution.java
@@ -9,13 +9,9 @@ public class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int n : nums) {
-            if (pq.size() < k) {
-                pq.add(n);
-            } else {
-                if (n > pq.peek()) {
-                    pq.remove();
-                    pq.add(n);
-                }
+            pq.add(n);
+            if (pq.size() > k) {
+                pq.remove();
             }
         }
         return pq.peek();
