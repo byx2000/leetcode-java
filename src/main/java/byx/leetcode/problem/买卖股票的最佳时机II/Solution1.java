5a0c0d0e658d35c5be8d9bc7ee8f69caa2e38f96
byx2000 <416982860@qq.com>
2021-10-08 12:28:34 +0800

    添加解法：买卖股票的最佳时机II

--- src/main/java/byx/leetcode/problem/买卖股票的最佳时机II/Solution1.java
+++ src/main/java/byx/leetcode/problem/买卖股票的最佳时机II/Solution1.java
@@ -3,7 +3,7 @@ package byx.leetcode.problem.买卖股票的最佳时机II;
 /**
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
  */
-public class Solution {
+public class Solution1 {
     public int maxProfit(int[] prices) {
         int result = 0;
         for (int i = 1; i < prices.length; ++i) {
@@ -15,7 +15,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution1 solution = new Solution1();
         System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
         System.out.println(solution.maxProfit(new int[]{1, 2, 3, 4, 5}));
         System.out.println(solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
