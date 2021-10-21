11378c92190dda2af21e663adfeb0ecfd24bcae5
byx2000 <416982860@qq.com>
2021-10-21 14:24:04 +0800

    修改题目：移除元素

--- src/main/java/byx/leetcode/problem/移除元素/Solution.java
+++ src/main/java/byx/leetcode/problem/移除元素/Solution.java
@@ -5,12 +5,11 @@ package byx.leetcode.problem.移除元素;
  */
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0, k = 0;
-        while (i < nums.length) {
+        int k = 0;
+        for (int i = 0; i < nums.length; ++i) {
             if (nums[i] != val) {
                 nums[k++] = nums[i];
             }
-            i++;
         }
         return k;
     }
