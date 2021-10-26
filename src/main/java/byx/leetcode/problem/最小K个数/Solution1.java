07c260ac64de44db58525d156512f98b470a3198
byx2000 <416982860@qq.com>
2021-10-26 09:29:48 +0800

    添加解法：最小K个数

--- src/main/java/byx/leetcode/problem/最小K个数/Solution1.java
+++ src/main/java/byx/leetcode/problem/最小K个数/Solution1.java
@@ -1,12 +1,11 @@
 package byx.leetcode.problem.最小K个数;
 
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
 /**
  * https://leetcode-cn.com/problems/smallest-k-lcci/
  */
-public class Solution {
+public class Solution1 {
     public int[] smallestK(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         for (int n : nums) {
@@ -17,10 +16,5 @@ public class Solution {
         }
 
         return pq.stream().mapToInt(n -> n).toArray();
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.smallestK(new int[]{1, 3, 5, 7, 2, 4, 6, 8}, 4)));
     }
 }
